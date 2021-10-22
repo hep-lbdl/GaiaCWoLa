0bd7698518b945007059e7270bebefc62a7ad0fb
Mariel Pettee <mariel.pettee@gmail.com>
2021-10-22 21:04:10 +0000

    Added training script

--- functions.py
+++ functions.py
@@ -1,34 +1,77 @@
+import os
 import random 
 from glob import glob
 import pandas as pd 
 import numpy as np
+import matplotlib.pyplot as plt
+from tqdm import tqdm 
+
+### Plot setup
+plt.rcParams.update({
+    'figure.dpi': 150,
+    "text.usetex": True,
+    "pgf.rcfonts": False,
+    "font.family": "serif",
+    "font.size": 15,
+    "xtick.labelsize": 11,
+    "ytick.labelsize": 11,
+    "legend.fontsize": 11
+})
 
 def get_random_file():
     file = random.choice(glob("/data0/mpettee/gaia_data/mock_streams/*.npy"))
     return(file)
 
-def load_file(file, stream = None, percent_bkg = 100):
+def angular_distance(angle1,angle2):
+    # inputs are np arrays of [ra,dec]
+    deltara=np.minimum(np.minimum(np.abs(angle1[:,0]-angle2[:,0]+360),np.abs(angle1[:,0]-angle2[:,0])),np.abs(angle1[:,0]-angle2[:,0]-360))
+    deltadec=np.abs(angle1[:,1]-angle2[:,1])
+    return np.sqrt(deltara**2+deltadec**2)
+
+#function from David's file via_machinae.py
+def FilterGD1(stars, gd1):
+    gd1stars=np.zeros(len(stars))
+    for x in tqdm(gd1):
+        ra=x[0]
+        dec=x[1]
+        pmra=x[2]
+        pmdec=x[3]
+        foundlist=angular_distance(np.dstack((stars[:,3],stars[:,2]))[0],np.array([[ra,dec]]))
+        foundlist=np.sqrt(foundlist**2+(stars[:,0]-pmdec)**2+(stars[:,1]-pmra)**2)   
+        foundlist=foundlist<.0001
+        if len(np.argwhere(foundlist))>1:
+            print(foundlist)
+        if len(np.argwhere(foundlist))==1:
+            gd1stars+=foundlist
+    gd1stars=gd1stars.astype('bool')
+    return gd1stars
+
+def load_file(stream = None, percent_bkg = 100):
     ### Stream options: ["mock", "gd1", "fjorm"]
     if stream == "mock": 
+        file = get_random_file()
         df = pd.DataFrame(np.load(file), columns=['μ_δ','μ_α','δ','α','mag','color','a','b','c','d','stream'])
         df['stream'] = df['stream']/100
         df['stream'] = df['stream'].astype(bool)
 
-        ### Drop any rows containing a NaN value
-        df.dropna(inplace = True)
-
-        ### Restrict data to a radius of 15
-        center_α = 0.5*(df.α.min()+df.α.max())
-        center_δ = 0.5*(df.δ.min()+df.δ.max())
-        df = df[np.sqrt((df.δ-center_δ)**2+(df.α-center_α)**2) < 15]
-
-        ### Construct stream DataFrame
-        stream = df[df.stream == True]
-        
     elif stream == "gd1": 
         print("Load GD1")
         df = pd.DataFrame()
         
+    elif stream == "gd1_tail":
+        file = "/data0/mpettee/gaia_data/gd1_tail/gaiascan_l101.2_b58.4_ra212.7_dec55.2.npy"
+        gd1 = np.load('/data0/mpettee/gaia_data/gd1/gd1_stars.npy')
+        df = pd.DataFrame(np.load(file)[:,[9,8,6,7,4,5]], columns=['μ_δ','μ_α','δ','α','color','mag'])
+        df['stream'] = FilterGD1(np.load(file), gd1)
+        
+    ### Drop any rows containing a NaN value
+    df.dropna(inplace = True)
+
+    ### Restrict data to a radius of 15
+    center_α = 0.5*(df.α.min()+df.α.max())
+    center_δ = 0.5*(df.δ.min()+df.δ.max())
+    df = df[np.sqrt((df.δ-center_δ)**2+(df.α-center_α)**2) < 15]        
+    
     if percent_bkg != 100:
         ### Optional: reduce background
         n_sig = len(df[df.stream == True])
@@ -38,5 +81,41 @@ def load_file(file, stream = None, percent_bkg = 100):
         events_to_drop = np.random.choice(df[df.stream == False].index, int((1-percent_bkg/100)*n_bkg), replace=False)
         df = df.drop(events_to_drop)
         print("After reduction, stream stars make up {:.3f}% of the dataset.".format(100*n_sig/len(df)))
-        
+    
     return df
+
+def visualize_stream(df, save_label=None):
+    plt.figure(figsize=(3,3), tight_layout=True) 
+    plt.hist2d(df.α,df.δ,bins=100)
+    if "stream" in df.keys():
+        plt.scatter(df[df.stream == True].α,df[df.stream == True].δ,color='white',s=0.2, label="Stream")
+    plt.xlabel(r"$\alpha$ [\textdegree]")
+    plt.ylabel(r"$\delta$ [\textdegree]");
+#     plt.legend();
+    if save_label is not None:
+        os.makedirs(os.path.join("./plots",save_label), exist_ok=True)
+        plt.savefig(os.path.join("./plots",save_label,"stream_position.png"))
+    
+    bins = np.linspace(-25,10,100)
+    if "stream" in df.keys():
+         fig, axs = plt.subplots(nrows=1, ncols=2, figsize=(6,3), tight_layout=True)
+    else:
+         fig, ax = plt.subplots(nrows=1, ncols=1, figsize=(3,3), tight_layout=True)
+    
+    if "stream" in df.keys(): ax = axs[0]
+    ax.hist2d(df.μ_α*np.cos(df.δ),df.μ_δ, bins=[bins,bins])
+    ax.set_xlabel(r"$\mu_\alpha\cos(\delta)$ [$\mu$as/year]", fontsize=11)
+    ax.set_ylabel(r"$\mu_\delta$ [$\mu$as/year]", fontsize=11);
+    ax.set_title("Background")
+    
+    if "stream" in df.keys():
+        ax = axs[1]
+        ax.hist2d(df[df.stream == True].μ_α*np.cos(df[df.stream == True].δ),df[df.stream == True].μ_δ, bins=[bins,bins])
+        ax.set_xlabel(r"$\mu_\alpha\cos(\delta)$ [$\mu$as/year]", fontsize=11)
+        ax.set_ylabel(r"$\mu_\delta$ [$\mu$as/year]", fontsize=11);
+        ax.set_title("Stream");
+        
+    if save_label is not None:
+        plt.savefig(os.path.join("./plots",save_label,"stream_velocities.png"))
+        
+    
---
+++ train.py
@@ -0,0 +1,246 @@
+### Generic imports 
+import os
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from matplotlib import gridspec
+from scipy import stats
+from glob import glob
+import argparse
+
+### ML imports
+from keras.layers import Input, Dense, Dropout
+from keras.models import Model, Sequential
+from keras import callbacks, regularizers
+from sklearn.metrics import roc_curve, auc,roc_auc_score
+from sklearn.model_selection import train_test_split
+from sklearn import preprocessing
+import tensorflow as tf
+
+### Custom imports
+from functions import *
+
+### GPU Setup
+os.environ["CUDA_VISIBLE_DEVICES"] = "0" # pick a number < 4 on ML4HEP; < 3 on Voltan 
+physical_devices = tf.config.list_physical_devices('GPU') 
+tf.config.experimental.set_memory_growth(physical_devices[0], True)
+
+### Plot setup
+plt.rcParams.update({
+    'figure.dpi': 150,
+    "text.usetex": True,
+    "pgf.rcfonts": False,
+    "font.family": "serif",
+    "font.size": 15,
+    "xtick.labelsize": 11,
+    "ytick.labelsize": 11,
+    "legend.fontsize": 11
+})
+
+def get_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("stream", default=None, choices = ["gd1", "gd1_tail", "mock"], help="Choose which stream to analyze.")
+    parser.add_argument("--percent_bkg", default=100, type=int, help="Percent of background to train on.")
+    parser.add_argument("--layer_size", default=128, type=int, help="Number of nodes per layer.")
+    parser.add_argument("--epochs", default=200, type=int, help="Number of training epochs.")
+    parser.add_argument("--batch_size", default=32, type=int, help="Batch size during training.")
+    parser.add_argument("--dropout", default=0, type=float, help="Dropout probability.")
+    parser.add_argument("--l2_reg", default=0, type=float, help="L2 regularization.")
+    return parser.parse_args()
+
+if __name__ == "__main__":
+    args = get_args()
+    
+    save_label = args.stream
+    
+    ### Load file & preprocess
+    df = load_file(stream = args.stream, percent_bkg = args.percent_bkg)
+    visualize_stream(df, save_label = save_label)
+    
+    ### Define signal & sideband regions 
+    if args.stream == "gd1_tail":
+        sb_min = -6
+        sb_max = 0
+        sr_min = -4.5
+        sr_max = -1.7
+
+    df_slice = df[(df.μ_δ > sb_min) & (df.μ_δ < sb_max)]
+    df_slice['label'] = np.where(((df_slice.μ_δ > sr_min) & (df_slice.μ_δ < sr_max)), 1, 0)
+    
+    plt.figure(figsize=(4,3), tight_layout=True)
+    bins = np.linspace(sb_min,sb_max,100)
+    plt.hist(df_slice[df_slice.label == 1].μ_δ,bins=bins,color="dodgerblue",label="Signal Region")
+    plt.hist(df_slice[df_slice.label == 0].μ_δ,bins=bins,color="orange",label="Sideband Region")
+    plt.legend(frameon=False)
+    plt.xlabel(r"$\mu_\delta$ [$\mu$as/year]")
+    plt.ylabel("Counts")
+    plt.savefig(os.path.join("./plots",save_label,"signal_sideband.png"))
+
+    sr = df_slice[df_slice.label == 1]
+    sb = df_slice[df_slice.label == 0]
+
+    print("Signal region has {:,} stream and {:,} bkg events.".format(sr.stream.value_counts()[True], sr.stream.value_counts()[False]))
+    print("Sideband region has {:,} stream and {:,} bkg events.".format(sb.stream.value_counts()[True], sb.stream.value_counts()[False]))
+    print("Total counts: SR = {:,}, SB = {:,}".format(len(sr), len(sb)))
+
+    ### Prepare datasets for training
+    training_vars = ['μ_α','δ','α','color','mag']
+
+    # 70/15/15 train/validate/test split
+    train, validate, test = np.split(df_slice.sample(frac=1), [int(.7*len(df_slice)), int(.85*len(df_slice))])
+
+    x_train, x_val, x_test = [train[training_vars], validate[training_vars], test[training_vars]]
+    y_train, y_val, y_test = [train.label, validate.label, test.label]
+
+    from sklearn.preprocessing import StandardScaler
+    sc = StandardScaler()
+    x_train = sc.fit_transform(x_train)
+    x_test = sc.transform(x_test)
+    x_val = sc.transform(x_val)
+    
+    ### Define model architecture 
+    save_name = "best_weights"+args.stream
+
+    model = Sequential()
+    if args.l2_reg == 0: 
+        reg = None
+    else:
+        reg = regularizers.l2(args.l2_reg)
+    
+    model.add(Dense(args.layer_size, input_dim=len(training_vars), activation='relu',
+                   activity_regularizer=reg
+                   )) 
+    if args.dropout != 0: 
+        model.add(Dropout(args.dropout))
+    model.add(Dense(args.layer_size, activation='relu',
+                   activity_regularizer=reg
+                   ))
+    if args.dropout != 0: 
+        model.add(Dropout(args.dropout))
+    model.add(Dense(args.layer_size, activation='relu',
+                   activity_regularizer=reg
+                   ))
+    if args.dropout != 0: 
+        model.add(Dropout(args.dropout))
+    model.add(Dense(1, activation='sigmoid'))
+    model.compile(loss='binary_crossentropy', 
+                  optimizer='adam', 
+                  metrics=['accuracy'])
+    model.summary()
+    
+    # stops if val_loss doesn't improve for [patience] straight epochs
+    early_stopping = callbacks.EarlyStopping(monitor='val_loss', 
+                                             patience=20, 
+                                             verbose=1) 
+
+    # saves weights from the epoch with lowest val_loss 
+    os.makedirs("./weights", exist_ok=True)
+    checkpoint = callbacks.ModelCheckpoint("./weights/"+save_name+".h5", 
+                                           monitor='val_loss', 
+                                           mode='auto', 
+                                           verbose=1, 
+                                           save_best_only=True, 
+                                           save_weights_only=True)
+
+    ### Train!
+    history = model.fit(x_train, y_train, 
+                        epochs=args.epochs, 
+                        batch_size=args.batch_size,
+                        validation_data=(x_val,y_val),
+                        callbacks = [
+                                    checkpoint, 
+                                    early_stopping],
+                        verbose = 2,
+                       )
+    
+    ### Evaluate training
+    model.load_weights("./weights/"+save_name+".h5")
+
+    ### Add the NN prediction score to the test set: 
+    test["nn_score"] = model.predict(x_test)
+    fake_eff_baseline, real_eff_baseline, thresholds = roc_curve(np.asarray(y_test), test.nn_score)
+    auc_baseline = auc(fake_eff_baseline, real_eff_baseline)
+    print("AUC: {}".format(auc_baseline))
+
+    ### Plot scores:
+    fig, axs = plt.subplots(nrows=1, ncols=2, figsize=(8,3), constrained_layout=True)
+    bins=np.linspace(0,1,10)
+
+    ax = axs[0]
+    ax.tick_params(labelsize=12)
+    ax.hist(test[test.label == 1].nn_score, bins=bins, histtype='step', linewidth=2, color="dodgerblue", label="Signal Region")
+    ax.hist(test[test.label == 0].nn_score, bins=bins, histtype='step', linewidth=2, color="orange", label="Sideband Region")
+    ax.legend(fontsize=12)
+    ax.set_xlim(0, 1)
+    ax.set_title("Test Set (15\% of full dataset)")
+    ax.set_xlabel("NN Score", size=12)
+    ax.set_ylabel("Events", size=12)
+
+    ax = axs[1]
+    ax.tick_params(labelsize=12)
+    ax.hist(test[test.stream == False].nn_score, bins=bins, histtype='step', linewidth=2, color="grey", label="Not Stream")
+    ax.hist(test[test.stream == True].nn_score, 
+            bins=bins, histtype='step', linewidth=2, color="crimson", label="Stream")
+    ax.legend(fontsize=12)
+    ax.set_yscale("log")
+    ax.set_xlim(0, 1)
+    ax.set_title("Test Set (15\% of full dataset)")
+    ax.set_xlabel("NN Score", size=12)
+    ax.set_ylabel("Events", size=12);
+    plt.savefig(os.path.join("./plots",save_label,"nn_scores.png"))
+    
+    ### Plot purities
+    # Scan for optimal percentage
+    cuts = np.linspace(0.1, 50, 100)
+    efficiencies = []
+    purities = []
+    for x in cuts:
+        top_stars = test[(test['nn_score'] >= test['nn_score'].quantile((100-x)/100))]
+        if True in top_stars.stream.unique():
+            n_perfect_matches = top_stars.stream.value_counts()[True]
+            stream_stars_in_test_set = test[test.stream == True]
+            efficiencies.append(100*n_perfect_matches/len(stream_stars_in_test_set))
+            purities.append(n_perfect_matches/len(top_stars)*100)
+        else:
+            efficiencies.append(np.nan)
+            purities.append(np.nan)
+
+    ### Choose a cut to optimize purity
+    print("Maximum purity of {:.1f}% at {:.1f}%".format(np.nanmax(purities),cuts[np.nanargmax(purities)]))
+    cut = cuts[np.nanargmax(purities)]
+    plt.plot(cuts, purities, label="Signal Purity")
+    plt.xlabel("Top \% Stars, ranked by NN score")
+    plt.legend()    
+    
+    ### Plot highest-ranked stars
+    x = 1 # desired percentage
+    top_stars = test[(test['nn_score'] >= test['nn_score'].quantile((100-x)/100))]
+    
+#     N = 20
+#     top_stars = test.sort_values(by=["nn_score"],ascending=False)[:N]
+    
+    n_perfect_matches = top_stars.stream.value_counts()[True]
+    stream_stars_in_test_set = test[test.stream == True]
+
+    print("Efficiency: {:.1f}%".format(100*n_perfect_matches/len(stream_stars_in_test_set)))
+    print("Purity: {:.1f}%".format(n_perfect_matches/len(top_stars)*100))
+    
+    plt.figure(figsize=(3,3), tight_layout=True) 
+    plt.scatter(stream_stars_in_test_set.α, stream_stars_in_test_set.δ, marker='.', 
+                color = "lightgray",
+                label='GD1 Tail')
+    plt.scatter(top_stars.α, top_stars.δ, marker='.', 
+                color = "lightpink",
+#                 label = 'Top {} stars'.format(N))
+                label='Top {:.0f}\% NN Scores'.format(x))
+    plt.scatter(top_stars[top_stars.stream].α, top_stars[top_stars.stream].δ, marker='.', 
+                color = "crimson",
+                label='Matches')
+    plt.legend()
+    plt.xlim(-15,15)
+    plt.ylim(-15,15)
+    plt.xlabel(r"$\alpha$ [\textdegree]")
+    plt.ylabel(r"$\delta$ [\textdegree]")
+    plt.savefig(os.path.join("./plots",save_label,"top_{}\%_stars.png".format(x)))
+
+    
