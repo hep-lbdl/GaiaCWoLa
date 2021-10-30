6fe6c00016fd72490075320771ae5fdf588ec8eb
Mariel Pettee <mariel.pettee@gmail.com>
2021-10-30 00:11:05 +0000

    Changed some of the sideband regions

--- functions.py
+++ functions.py
@@ -90,12 +90,17 @@ def load_file(stream = None, percent_bkg = 100):
         df = df.drop(events_to_drop)
         print("After reduction, stream stars make up {:.3f}% of the dataset.".format(100*n_sig/len(df)))
     
+    df.reset_index(inplace=True)
     return df
 
-def visualize_stream(df, save_folder=None):
-    plt.figure(figsize=(3,3), tight_layout=True)
-    plt.hist2d(df.α,df.δ,bins=50)
-    if "stream" in df.keys():
+def visualize_stream(df, type="hist", show_stream=True, save_folder=None):
+    plt.figure(figsize=(3.7,3), tight_layout=True)
+    if type == "scatter":
+        plt.scatter(df.α,df.δ, marker='.', s=0.1, c=df["b-r"])
+    else:
+        plt.hist2d(df.α,df.δ,bins=50)
+        plt.colorbar()
+    if "stream" in df.keys() and show_stream==True:
         plt.scatter(df[df.stream == True].α,df[df.stream == True].δ,color='white',s=0.2, label="Stream")
     plt.xlabel(r"$\alpha$ [\textdegree]")
     plt.ylabel(r"$\delta$ [\textdegree]");
@@ -127,10 +132,10 @@ def visualize_stream(df, save_folder=None):
         
 def signal_sideband(df, stream, save_folder=None):
     if stream == "gd1_tail":
-        sb_min = -6
+        sb_min = -7.5
         sb_max = 0
-        sr_min = -4.5
-        sr_max = -1.7
+        sr_min = -5
+        sr_max = -1.5
         
     elif stream == "mock":
         sb_min = df[df.stream].μ_δ.mean()-df[df.stream].μ_δ.std()/2
@@ -138,12 +143,20 @@ def signal_sideband(df, stream, save_folder=None):
         sr_min = df[df.stream].μ_δ.mean()-df[df.stream].μ_δ.std()/4
         sr_max = df[df.stream].μ_δ.mean()+df[df.stream].μ_δ.std()/4
 
-    else: 
-        sb_min = df.μ_δ.mean()-df.μ_δ.std()/2
-        sb_max = df.μ_δ.mean()+df.μ_δ.std()/2
-        sr_min = df.μ_δ.mean()-df.μ_δ.std()/4
-        sr_max = df.μ_δ.mean()+df.μ_δ.std()/4
+    elif stream == "gd1": 
+        sb_min = -18
+        sb_max = -9.5
+        sr_min = -15
+        sr_max = -11
         
+    else: 
+        sb_min = df.μ_δ.min()
+        sb_max = df.μ_δ.max()
+        sr_min = sb_min+2.5
+        sr_max = sb_max-2.5
+    
+    print(sb_min, sr_min, sr_max, sb_max)
+    
     df_slice = df[(df.μ_δ > sb_min) & (df.μ_δ < sb_max)]
     df_slice['label'] = np.where(((df_slice.μ_δ > sr_min) & (df_slice.μ_δ < sr_max)), 1, 0)
     
@@ -162,15 +175,25 @@ def signal_sideband(df, stream, save_folder=None):
     print("Total counts: SR = {:,}, SB = {:,}".format(len(sr), len(sb)))
 
     if "stream" in df.keys():
-        print("Signal region has {:,} stream and {:,} bkg events.".format(sr.stream.value_counts()[True], sr.stream.value_counts()[False]))
-        print("Sideband region has {:,} stream and {:,} bkg events.".format(sb.stream.value_counts()[True], sb.stream.value_counts()[False]))
+        try: n_sig_stream_stars = sr.stream.value_counts()[True]
+        except: n_sig_stream_stars = 0
+        try: n_sideband_stream_stars = sb.stream.value_counts()[True]
+        except: n_sideband_stream_stars = 0
+        try: n_sig_bkg_stars = sr.stream.value_counts()[False]
+        except: n_sig_bkg_stars = 0
+        try: n_sideband_bkg_stars = sb.stream.value_counts()[False]
+        except: n_sideband_bkg_stars = 0
+            
+        print("Signal region has {:,} stream and {:,} bkg events.".format(n_sig_stream_stars, n_sig_bkg_stars))
+        print("Sideband region has {:,} stream and {:,} bkg events.".format(n_sideband_stream_stars, n_sideband_bkg_stars))
         
     return df_slice
 
 def plot_results(test, save_folder=None):
+    if save_folder is not None: 
+        os.makedirs(save_folder, exist_ok=True)
     fig, axs = plt.subplots(nrows=1, ncols=2, figsize=(8,3), constrained_layout=True)
     bins=np.linspace(0,1,10)
-
     ax = axs[0]
     ax.tick_params(labelsize=12)
     ax.hist(test[test.label == 1].nn_score, bins=bins, histtype='step', linewidth=2, color="dodgerblue", label="Signal Region")
@@ -181,74 +204,78 @@ def plot_results(test, save_folder=None):
     ax.set_xlabel("NN Score", size=12)
     ax.set_ylabel("Events", size=12)
 
-    ax = axs[1]
-    ax.tick_params(labelsize=12)
-    ax.hist(test[test.stream == False].nn_score, bins=bins, histtype='step', linewidth=2, color="grey", label="Not Stream")
-    ax.hist(test[test.stream == True].nn_score, 
-            bins=bins, histtype='step', linewidth=2, color="crimson", label="Stream")
-    ax.legend(fontsize=12)
-    ax.set_yscale("log")
-    ax.set_xlim(0, 1)
-    ax.set_title("Test Set (15\% of full dataset)")
-    ax.set_xlabel("NN Score", size=12)
-    ax.set_ylabel("Events", size=12);
+    if "stream" in test.keys():
+        ax = axs[1]
+        ax.tick_params(labelsize=12)
+        ax.hist(test[test.stream == False].nn_score, bins=bins, histtype='step', linewidth=2, color="grey", label="Not Stream")
+        ax.hist(test[test.stream == True].nn_score, 
+                bins=bins, histtype='step', linewidth=2, color="crimson", label="Stream")
+        ax.legend(fontsize=12)
+        ax.set_yscale("log")
+        ax.set_xlim(0, 1)
+        ax.set_title("Test Set (15\% of full dataset)")
+        ax.set_xlabel("NN Score", size=12)
+        ax.set_ylabel("Events", size=12);
     if save_folder is not None: 
         plt.savefig(os.path.join(save_folder,"nn_scores.png"))
     
     ### Plot purities
-    # Scan for optimal percentage
-    cuts = np.linspace(0.01, 50, 100)
-    efficiencies = []
-    purities = []
-    for x in cuts:
-        top_stars = test[(test['nn_score'] >= test['nn_score'].quantile((100-x)/100))]
-        if True in top_stars.stream.unique():
-            n_perfect_matches = top_stars.stream.value_counts()[True]
-            stream_stars_in_test_set = test[test.stream == True]
-            efficiencies.append(100*n_perfect_matches/len(stream_stars_in_test_set))
-            purities.append(n_perfect_matches/len(top_stars)*100)
-        else:
-            efficiencies.append(np.nan)
-            purities.append(np.nan)
+    if "stream" in test.keys():
+        # Scan for optimal percentage
+        cuts = np.linspace(0.01, 50, 100)
+        efficiencies = []
+        purities = []
+        for x in cuts:
+            top_stars = test[(test['nn_score'] >= test['nn_score'].quantile((100-x)/100))]
+            if True in top_stars.stream.unique():
+                n_perfect_matches = top_stars.stream.value_counts()[True]
+                stream_stars_in_test_set = test[test.stream == True]
+                efficiencies.append(100*n_perfect_matches/len(stream_stars_in_test_set))
+                purities.append(n_perfect_matches/len(top_stars)*100)
+            else:
+                efficiencies.append(np.nan)
+                purities.append(np.nan)
 
-    ### Choose a cut to optimize purity
-    print("Maximum purity of {:.1f}% at {:.2f}%".format(np.nanmax(purities),cuts[np.nanargmax(purities)]))
-    cut = cuts[np.nanargmax(purities)]
-    plt.figure()
-    plt.plot(cuts, purities, label="Signal Purity")
-    plt.xlabel("Top \% Stars, ranked by NN score")
-    plt.legend()    
-    if save_folder is not None: 
-        plt.savefig(os.path.join(save_folder,"purities.png"))
+        ### Choose a cut to optimize purity
+        if not np.isnan(purities).all():
+            print("Maximum purity of {:.1f}% at {:.2f}%".format(np.nanmax(purities),cuts[np.nanargmax(purities)]))
+            cut = cuts[np.nanargmax(purities)]
+            plt.figure()
+            plt.plot(cuts, purities, label="Signal Purity")
+            plt.xlabel("Top \% Stars, ranked by NN score")
+            plt.legend()    
+            if save_folder is not None: 
+                plt.savefig(os.path.join(save_folder,"purities.png"))
 
     ### Plot highest-ranked stars
     for x in [0.001, 0.01, 0.1, 1, 5, 10, 20]: # percentages
         top_stars = test[(test['nn_score'] >= test['nn_score'].quantile((100-x)/100))]
-        stream_stars_in_test_set = test[test.stream == True]
-
-        if True in top_stars.stream.unique(): 
-            n_perfect_matches = top_stars.stream.value_counts()[True] 
-        else: 
-            n_perfect_matches = 0 
-            print("No stream matches identified in top star selection.")
+        if "stream" in test.keys():
+            stream_stars_in_test_set = test[test.stream == True]
+            if True in top_stars.stream.unique(): 
+                n_perfect_matches = top_stars.stream.value_counts()[True] 
+            else: 
+                n_perfect_matches = 0 
         
-        print("Cut at {}%...".format(x))
-        print("Efficiency: {:.1f}%".format(100*n_perfect_matches/len(stream_stars_in_test_set)))
-        print("Purity: {:.1f}%".format(n_perfect_matches/len(top_stars)*100))
+            print("Top {}% stars: Purity = {:.1f}% ".format(x,n_perfect_matches/len(top_stars)*100))
 
         plt.figure(figsize=(5,3), tight_layout=True) 
-        plt.title('Top {:.2f}\% NN Scores'.format(x))
-#         plt.title("Purity = {:.0f}\%".format(n_perfect_matches/len(top_stars)*100), fontsize=11)
-        plt.scatter(stream_stars_in_test_set.α, stream_stars_in_test_set.δ, marker='.', 
+        plt.title('Top {:.3f}\% NN Scores'.format(x))
+        if "stream" in test.keys():
+            plt.scatter(stream_stars_in_test_set.α, stream_stars_in_test_set.δ, marker='.', 
                     color = "lightgray",
                     label='Stream')
-        plt.scatter(top_stars.α, top_stars.δ, marker='.', 
+            plt.scatter(top_stars.α, top_stars.δ, marker='.', 
                     color = "lightpink",
                     label="Top Stars\n(Purity = {:.0f}\%)".format(n_perfect_matches/len(top_stars)*100))
-        if True in top_stars.stream.unique(): 
-            plt.scatter(top_stars[top_stars.stream].α, top_stars[top_stars.stream].δ, marker='.', 
+            if True in top_stars.stream.unique(): 
+                plt.scatter(top_stars[top_stars.stream].α, top_stars[top_stars.stream].δ, marker='.', 
+                        color = "crimson",
+                        label='Matches')
+        else:
+            plt.scatter(top_stars.α, top_stars.δ, marker='.', 
                     color = "crimson",
-                    label='Matches')
+                    label="Top Stars") 
         plt.legend(bbox_to_anchor=(1.1, 1), loc='upper left')
         plt.xlim(-15,15)
         plt.ylim(-15,15)
