7c48a4ec877bed9839e72fda195f93eec0ec461b
Mariel Pettee <mariel.pettee@gmail.com>
2022-01-07 22:28:39 -0800

    Optimizing SR/SB region limits

--- functions.py
+++ functions.py
@@ -86,13 +86,10 @@ def load_file(stream = None, folder = "./gaia_data/", percent_bkg = 100):
                            'streammask': 'stream'}, inplace=True)
 
     elif stream == "gd1_tail":
-#         file = os.path.join(folder,"gd1_tail/gd1_tail.h5")
-#         df = pd.read_hdf(file)
+        file = os.path.join(folder,"gd1_tail/gd1_tail.h5")
+#         file = os.path.join(folder,"gd1_tail/gd1_tail_optimized_patch.h5")
+        df = pd.read_hdf(file)
 
-        file = "gd1_allpatches.h5"
-        df_all = pd.read_hdf(file)
-        df = df_all[(df_all.α > 185) & (df_all.α < 230)] # select just the tail area 
-        
     elif stream == "gaia3":
         ### Note that we don't have stream labels here
         file = get_random_file(os.path.join(folder,"gaia3/*.npy"))
@@ -203,11 +200,11 @@ def signal_sideband(df, stream=None, save_folder=None, sb_min=None, sb_max=None,
         # sr_min = -10 
         # sr_max = -8 
         
-#         ### My optimized limits (hand-chosen)
-#         sb_min = -15
-#         sr_min = -11
-#         sr_max = -7
-#         sb_max = -5
+        ### My optimized limits (hand-chosen)
+        sb_min = -12 # -15
+        sr_min = -11
+        sr_max = -8 # -7
+        sb_max = -6 # -5
         
         # ### All signal in the SR
         # sb_min = -15
@@ -221,11 +218,11 @@ def signal_sideband(df, stream=None, save_folder=None, sb_min=None, sb_max=None,
         # sr_max = -6
         # sb_max = -3
         
-        # ### GD1 tail w/ overlapping patches 
-        sb_min = -8.5
-        sr_min = -5
-        sr_max = -1
-        sb_max = 0
+        ### Optimized GD1 tail w/ overlapping patches 
+#         sb_min = -7
+#         sr_min = -6
+#         sr_max = -3.1
+#         sb_max = -3
         
     elif stream == "mock":
         sb_min = df[df.stream].μ_δ.mean()-df[df.stream].μ_δ.std()/2
@@ -369,7 +366,7 @@ def plot_results(test, save_folder=None):
                 plt.savefig(os.path.join(save_folder,"purities.png"))
 
     ### Plot highest-ranked stars
-    for x in [10, 100]: # top N stars
+    for x in [10, 20, 50, 100]: # top N stars
         top_stars = test.sort_values('nn_score',ascending=False)[:x]
         if "stream" in test.keys():
             stream_stars_in_test_set = test[test.stream == True]
@@ -404,40 +401,22 @@ def plot_results(test, save_folder=None):
         plt.ylabel(r"$\delta$ [\textdegree]")
         if save_folder is not None: 
             plt.savefig(os.path.join(save_folder,"top_{}_stars.png".format(x)))
-
-#     print("===================")
-#     for x in [0.001, 0.01, 0.1, 1, 5, 10, 20]: # percentages
-#         top_stars = test[(test['nn_score'] >= test['nn_score'].quantile((100-x)/100))]
-#         if "stream" in test.keys():
-#             stream_stars_in_test_set = test[test.stream == True]
-#             if True in top_stars.stream.unique(): 
-#                 n_perfect_matches = top_stars.stream.value_counts()[True] 
-#             else: 
-#                 n_perfect_matches = 0 
-        
-#             print("Top {}% stars: Purity = {:.1f}% ".format(x,n_perfect_matches/len(top_stars)*100))
-
-#         plt.figure(figsize=(5,3), dpi=150, tight_layout=True) 
-#         plt.title('Top {:.3f}\% NN Scores'.format(x))
-#         if "stream" in test.keys():
-#             plt.scatter(stream_stars_in_test_set.α, stream_stars_in_test_set.δ, marker='.', 
-#                     color = "lightgray",
-#                     label='Stream')
-#             plt.scatter(top_stars.α, top_stars.δ, marker='.', 
-#                     color = "lightpink",
-#                     label="Top Stars\n(Purity = {:.0f}\%)".format(n_perfect_matches/len(top_stars)*100))
-#             if True in top_stars.stream.unique(): 
-#                 plt.scatter(top_stars[top_stars.stream].α, top_stars[top_stars.stream].δ, marker='.', 
-#                         color = "crimson",
-#                         label='Matches')
-#         else:
-#             plt.scatter(top_stars.α, top_stars.δ, marker='.', 
-#                     color = "crimson",
-#                     label="Top Stars") 
-#         plt.legend(bbox_to_anchor=(1.1, 1), loc='upper left')
-#         plt.xlim(test.α.min(),test.α.max())
-#         plt.ylim(test.δ.min(),test.δ.max())
-#         plt.xlabel(r"$\alpha$ [\textdegree]")
-#         plt.ylabel(r"$\delta$ [\textdegree]")
-#         if save_folder is not None: 
-#             plt.savefig(os.path.join(save_folder,"top_{}%_stars.png".format(x)))
+            
+        plt.figure(figsize=(5,3), dpi=150, tight_layout=True) 
+        plt.title('Top {} Stars'.format(x))
+        plt.scatter(stream_stars_in_test_set.α, stream_stars_in_test_set.δ, marker='.', 
+                    color = "lightgray",
+                    label='Stream')
+        plt.scatter(top_stars.α, top_stars.δ, marker='.', 
+        c = top_stars.nn_score,
+        cmap = "Blues",
+        vmin=top_stars.nn_score.min(),
+        vmax=top_stars.nn_score.max(),
+        label="Top Stars") 
+        plt.legend(bbox_to_anchor=(1.1, 1), loc='upper left')
+        plt.xlim(test.α.min(),test.α.max())
+        plt.ylim(test.δ.min(),test.δ.max())
+        plt.xlabel(r"$\alpha$ [\textdegree]")
+        plt.ylabel(r"$\delta$ [\textdegree]")
+        if save_folder is not None: 
+            plt.savefig(os.path.join(save_folder,"top_{}_stars_ranked.png".format(x)))
--- train.py
+++ train.py
@@ -21,6 +21,8 @@ from sklearn.metrics import roc_curve, auc,roc_auc_score
 from sklearn.model_selection import train_test_split
 from sklearn import preprocessing
 import tensorflow as tf
+import wandb
+from wandb.keras import WandbCallback
 
 ### Custom imports
 from functions import *
@@ -49,22 +51,28 @@ def get_args():
     parser.add_argument("--save_label", default='test', type=str, help="Folder name for saving training outputs & plots.")
     parser.add_argument("--percent_bkg", default=100, type=int, help="Percent of background to train on.")
     parser.add_argument("--layer_size", default=128, type=int, help="Number of nodes per layer.")
-    parser.add_argument("--patience", default=30, type=int, help="How many epochs of no val_loss improvement before the training is stopped.")
+    parser.add_argument("--patience", default=20, type=int, help="How many epochs of no val_loss improvement before the training is stopped.")
     parser.add_argument("--epochs", default=200, type=int, help="Number of training epochs.")
     parser.add_argument("--batch_size", default=32, type=int, help="Batch size during training.")
-    parser.add_argument("--dropout", default=0, type=float, help="Dropout probability.")
+    parser.add_argument("--dropout", default=0.2, type=float, help="Dropout probability.")
     parser.add_argument("--l2_reg", default=0, type=float, help="L2 regularization.")
     parser.add_argument("--n_folds", default=1, type=int, help="Number of k-folds.")
     parser.add_argument("--sample_weight", default=1, type=float, help="If not equal to 1, adds an additional weight to each star in the stream.")
     parser.add_argument('--remove_stream_sb', action='store_true', help="Use this if you want to remove stream stars from the sideband region.")
-    parser.add_argument("--best_of_n_loops", default=3, type=int, help="Repeats the training N times and picks the best weights.")
-
+    parser.add_argument("--best_of_n_loops", default=1, type=int, help="Repeats the training N times and picks the best weights.")
 
     return parser.parse_args()
 
 if __name__ == "__main__":
     args = get_args()
     t0 = time.time()
+    
+#     wandb.init(project="gd1-tail", entity="mpettee")
+#     wandb.config = {
+#       "epochs": args.epochs,
+#       "batch_size": args.batch_size,
+#       "layer_size": args.layer_size, 
+#     }
     
     save_label = args.save_label
     save_folder = os.path.join("./trained_models",save_label)
@@ -90,7 +98,9 @@ if __name__ == "__main__":
     df["weight"] = np.where(df['stream']==True, args.sample_weight, 1)
     
     ### Train
-    train(df, layer_size=args.layer_size, dropout=args.dropout, l2_reg=args.l2_reg, epochs=args.epochs, patience=args.patience, n_folds=args.n_folds, best_of_n_loops=args.best_of_n_loops, save_folder=save_folder)
+    train(df, layer_size=args.layer_size, dropout=args.dropout, l2_reg=args.l2_reg, epochs=args.epochs, patience=args.patience, n_folds=args.n_folds, best_of_n_loops=args.best_of_n_loops, save_folder=save_folder, 
+#           other_callbacks=[WandbCallback()]
+         )
     
     print("Finished in {:,.1f} seconds.".format(time.time() - t0))
           
